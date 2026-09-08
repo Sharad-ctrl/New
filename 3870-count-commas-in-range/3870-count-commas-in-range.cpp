27999be726374b09a7b20ac02e5e6d75ac0ca9f7
Sharad-ctrl <2k25csaiml2513871@gmail.com>
2026-09-08 17:52:07 +0530

    Time: 774 ms (5.13%), Space: 9.4 MB (5.52%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int countCommas(int n) {
-       return max(n-999,0);
+        int count = 0;
+        string s = to_string(n);
+        int m = s.size();
+        while(m>3){
+            if(m>4 && m%3 != 0 ) m = m-1;
+            if(m%3 != 0)  count = count+(m-4+1);
+            if(m%3 == 0) count = count + (m-4-1);
+            n = n-1;
+            s = to_string(n);
+            m = s.size();
+        }
+        return count;
     }
 };
