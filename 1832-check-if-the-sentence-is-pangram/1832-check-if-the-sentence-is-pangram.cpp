fa9a7e612f4ad03eb47b32dcf1292d8236ecf2a3
Sharad-ctrl <2k25csaiml2513871@gmail.com>
2026-09-23 22:15:15 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.07%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        set<int>st;
-        for(int i=0;i<sentence.size();i++){
-            st.insert(sentence[i]);
-        }
-        int n = st.size();
-        if(n < 26) return false;
-        else return true;
+       set<int> s;
+        for (auto& c: sentence)
+            s.insert(c);
+        return s.size() == 26;
     }
 };
